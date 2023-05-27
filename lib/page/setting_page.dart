282337556dead7a1752134affc47e4e8a07242bf
Haeseong Jeon <seong0428@gmail.com>
2023-05-28 00:26:16 +0900

    feat: 오픈소스 라이선스 추가

--- lib/page/setting_page.dart
+++ lib/page/setting_page.dart
@@ -7,15 +7,20 @@ class SettingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text('개인정보차리방침'),
-        Text('위치서비스이용약관'),
-        Text('서비스이용약관'),
-        Text('오픈소스라이선스'),
+        Text('서비스 이용약관'),
+        Text('개인정보 처리방침'),
+        Text('위치서비스 이용약관'),
+        TextButton(
+          onPressed: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (_) => const LicensePage()));
+          },
+          child: Text('오픈소스라이선스'),
+        ),
         Text('버전정보'),
         Text('로그아웃'),
         Text('회원탈퇴'),
       ],
     );
   }
-
 }
